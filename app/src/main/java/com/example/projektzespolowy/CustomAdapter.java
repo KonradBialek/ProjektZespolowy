0c879174ddd9d4feb4d449b0d69d84224487034b
Roksana <rgulewska@gmail.com>
2021-05-21 15:44:14 +0200

    dodałam awatara, logo i grafikę burgera z menu do drawable

---
+++ app/src/main/java/com/example/projektzespolowy/CustomAdapter.java
@@ -0,0 +1,54 @@
+package com.example.projektzespolowy;
+//implementacja customowego adaptera
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class CustomAdapter extends BaseAdapter { //dziedziczy po klasie BaseAdapter (jest to klasa takiego podstawowego
+// adaptera który można zastosować do np GridView albo do ListView)
+//przedstawia całe zaadoptowanie tego grida
+    private String[] plantsNames;
+    private int[] imagesPhoto;
+    private Context context;
+    private LayoutInflater layoutInflater;
+
+    public CustomAdapter(String[] imageNames, int[] imagesPhoto, Context context) {
+        this.plantsNames = imageNames;
+        this.imagesPhoto = imagesPhoto;
+        this.context = context;
+        this.layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public int getCount() {
+        return imagesPhoto.length;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        if(view == null){
+            view = layoutInflater.inflate(R.layout.plant_row_item, viewGroup, false);
+        }
+        TextView tvName = view.findViewById(R.id.minPlantName);
+        ImageView imagePhoto = view.findViewById(R.id.imageView);
+
+        tvName.setText(plantsNames[i]);
+        imagePhoto.setImageResource(imagesPhoto[i]);
+
+        return view;
+    }
+}
