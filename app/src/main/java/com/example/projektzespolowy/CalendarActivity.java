40d8888bcbe4a1b12755c08e3387150dfd99fdbf
Roksana <rgulewska@gmail.com>
2021-06-05 06:58:27 +0200

    poprawione, teraz się kompiluje

--- app/src/main/java/com/example/projektzespolowy/CalendarActivity.java
+++ app/src/main/java/com/example/projektzespolowy/CalendarActivity.java
@@ -29,73 +29,9 @@ public class CalendarActivity extends AppCompatActivity {
     ListView listView;
     ArrayAdapter<CalendarListViewItem> arrayAdapter;
 
-/*
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_calendar);
-
-        //inicjalizacja zdefiniowanych w tej klasie pól, łączenie pól z layoutem activity_calendar, za pomocą funkcji findViewById()
-        menu = (Button) findViewById(R.id.menu);
-        plantsCalendar = (CalendarView) findViewById(R.id.plantsCalendar);
-        listView = (ListView) findViewById(R.id.calListView);
 
 
-        plantsCalendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
-            @Override
-            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
-                String date = dayOfMonth + "/" + (month+1) + "/" + year;
-                calendarHeaderTextView.setText(date);
-                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-                    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/M/yyyy");
-                    selectedDate = LocalDate.parse(date,formatter);
-                }
-                Log.i("selectedDate: ",selectedDate.toString());
 
-                //TODO w tym miejscu pobieram z apki serwerowej liste kwiatow do podlania na konkretny dzien
-                //TODO muszę dopisac implementacje pobierania listy z apki serwerowej
-                CalendarListViewItem[] itemList = new CalendarListViewItem[2];
-                itemList[0] = new CalendarListViewItem(1L, "fajen kwiatek",true);
-                itemList[1] = new CalendarListViewItem(1L, "kolejny fajen kwiatek", false);
-
-
-                arrayAdapter = new ArrayAdapter<CalendarListViewItem>(getApplicationContext(), android.R.layout.simple_list_item_multiple_choice, itemList);
-                listView.setAdapter(arrayAdapter);
-
-                validateWateringOfItems(arrayAdapter, listView); // w tym miejscu w listView zaznaczane sa checkboxy dla kwiatkow z bazy ktore zostaly juz podlane
-
-                listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                    @Override
-                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                        if(listView.isItemChecked(position)){
-                            //TODO tutaj mogę wysylac zapis podlania do bazy danych (aplikacji serwerowej)
-                            Log.i("TODO", "zapisz w bazie podlanie");
-                            CalendarListViewItem item = (CalendarListViewItem) listView.getItemAtPosition(position);
-                            Log.i("it:",item.getName());
-                        }
-                        if(!listView.isItemChecked(position)){
-                            Log.i("ITEM:","Item zostal odklikniety");
-                            //TODO mogę dopisac w tym miejscu obsluge odklikniecia podlania
-                        }
-
-                    }
-                });
-            }
-        });
-
-    }
-
-
-    public void validateWateringOfItems(ArrayAdapter adapter, ListView listViewParam){
-        CalendarListViewItem tmp;
-        for(int i=0; i<adapter.getCount(); i++){
-            tmp = (CalendarListViewItem) listViewParam.getItemAtPosition(i);
-            if(tmp.isWatered()){
-                listViewParam.setItemChecked(i,true);
-            }
-        }
-    }
-    */
 
     public void Menu(View view) {
         Intent menuIntent = new Intent(getApplicationContext(), NavigationActivity.class);
--- app/src/main/java/com/example/projektzespolowy/UsersProfileActivity.java
+++ app/src/main/java/com/example/projektzespolowy/UsersProfileActivity.java
@@ -17,52 +17,6 @@ public class UsersProfileActivity extends AppCompatActivity {
     TextView userName;
     GridView gridView;
 
-    //tutaj tworzę nazwy i zdjęcia z ręki, ale domyślnie w ich miejsce przyjda nam z bazy danych                                    5
-    String[] plantsNames = {};
-    int[] plantsAvatars = {};
-
-    public static final int PLANTS_PROFILE_ACTIVITY_CODE = 111; //to jest ten kod zwracany przez startowaną aktywność w funkcji wyświetlającej profil rośliny
-
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_users_profile);
-
-        menu = (Button) findViewById(R.id.menu);
-        userName = (TextView) findViewById(R.id.userName);
-        gridView = (GridView) findViewById(R.id.gridView);
-
-        CustomAdapterUsersProfile customAdapterUsersProfile = new CustomAdapterUsersProfile(plantsNames, plantsAvatars, getApplicationContext());
-        //tworzymy customowy adapter, stworzony na podstawie tablicy naszych nazw zdjęć oraz na podstawie tablicy zdjęć i kontekstu
-        gridView.setAdapter(customAdapterUsersProfile); //ustawienie customowego adaptera na GridView
-
-        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener(){// listener który uruchamia się jak kliknę na któreś zdjęcie
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-
-                String selectedName = plantsNames[i];//wybieramy z naszej tablicy nazw i-ty element (i-ta nazwa jest przypisana do i-tego zdjęcia, wdzięki czemu klikając na i-ty element nie musimy
-                // pobierać jego nazwy, bo nazwę pobierzemy sobie z tablicy nazw jako i-tą)
-                int selectedAvatar = plantsAvatars[i];
-                Intent detailsImageIntent = new Intent(getApplicationContext(), PlantsProfileActivity.class);//tworzymy nową intencję
-                detailsImageIntent.putExtra("name",selectedName);
-                detailsImageIntent.putExtra("avatar",selectedAvatar);
-                startActivity(detailsImageIntent);//startujemy naszą aktywność
-            }
-        });
-    }
-
-    public void displayPlantsProfile(View view){
-
-
-
-    }
-
-
-
-
-
-
 
     public void Menu(View view) {
         Intent menuIntent = new Intent(getApplicationContext(), NavigationActivity.class);
